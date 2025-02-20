8625a963b7bde32704d3b641faf2a72110cb7241
canonil <161255119+canonil@users.noreply.github.com>
2025-02-20 18:15:51 +0100

    boring.js

--- boring.js
+++ boring.js
@@ -1,19 +1,24 @@
 let running = true
-let answer = "dark"
-let answer2 = "Dark"
+let answer = ["dark", "Dark"]
 let guess;
+let hint = false;
 
 while(running){
     guess = window.prompt("Who uses chatgdp for scirpt")
 
-    if(guess == answer){
+    if(guess.includes(answer[0]) || guess.includes(answer[1])){
         window.alert("correct😹😹")
         running = false;
     }
-    else if(guess == answer2)[
-        window.alert("correct😹😹")
-    ]
+    else if(guess !== answer && !hint){
+        window.alert("Hint: its dark😹")
+        hint = true;
+    }
+    else if(guess.includes(`no`)){
+        window.alert("Meeh L answer")
+    }
     else{
         window.alert("dumbass")
     }
 }
+console.log(answer)
